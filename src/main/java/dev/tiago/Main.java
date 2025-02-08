7873e5bf95b296c148d294d1bc6e78931a8d5860
tiago <tiago.rodrigues.tr99@hotmail.com>
2025-02-08 16:45:00 -0300

    ajustes add trim para remocao de espacos nos nomes e formatacao para diminuicao das casas decimais

--- src/main/java/dev/tiago/Main.java
+++ src/main/java/dev/tiago/Main.java
@@ -6,23 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String name;
-        double height;
-        double wight;
+        String nome;
+        double altura;
+        double peso;
 
         System.out.print("Digite seu nome: ");
-        name = sc.nextLine();
+        nome = sc.nextLine();
         System.out.print("Digite sua altura: ");
-        height = sc.nextDouble();
+        altura = sc.nextDouble();
         System.out.print("Digite seu peso: ");
-        wight = sc.nextDouble();
+        peso = sc.nextDouble();
+        System.out.println();
 
-        double imc = wight / (height*height);
+        String nameWithoutSpace = nome.trim();
+        double imc = peso / (altura*altura);
 
         if (imc < 18.5){
             System.out.println("Abaixo do peso");
         }else if (imc >= 18.6 && imc <= 24.9){
-            System.out.println("Peso normal");
+            System.out.println("De acordo com sua altura e peso, você está no peso ideal, parabéns!");
         }else if (imc >= 25 && imc <= 29.9){
             System.out.println("Acima do peso");
         }else if (imc >= 30 && imc <= 34.9){
@@ -35,7 +37,8 @@ public class Main {
             System.out.println("Obesidade mórdita");
         }
 
-        System.out.println(name + ", Seu IMC é: " + imc);
+        System.out.printf(nameWithoutSpace + ", seu IMC é: %.2f", imc);
+        System.out.println();
 
         sc.close();
     }
